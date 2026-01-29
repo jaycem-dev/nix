f8c0a563311d0247c9b09127993a9472468e589d
jaycem <45575946+jaycem-dev@users.noreply.github.com>
2026-01-29 14:11:51 -0300

    fix: use compositor arg for hypridle cmds, fixing waybar spawning multiple instances

--- home/hypridle.nix
+++ home/hypridle.nix
@@ -1,11 +1,14 @@
-_: {
+{ lib, compositor, ... }: {
   services.hypridle = {
     enable = true;
     settings = {
       general = {
         lock_cmd = "pidof hyprlock || hyprlock"; # avoid starting multiple hyprlock instances.
         before_sleep_cmd = "loginctl lock-session"; # lock before suspend.
-        # after_sleep_cmd = "hyprctl dispatch dpms on || niri msg action power-on-monitors"; # to avoid having to press a key twice to turn on the display.
+        after_sleep_cmd = lib.mkMerge [
+          (lib.mkIf (compositor == "hyprland") "hyprctl dispatch dpms on")
+          (lib.mkIf (compositor == "niri") "niri msg action power-on-monitors")
+        ]; # to avoid having to press a key twice to turn on the display.
       };
 
       listener = [
@@ -27,11 +30,16 @@ _: {
           on-timeout = "loginctl lock-session"; # lock screen when timeout has passed
         }
 
-        {
+        (lib.mkIf (compositor == "hyprland") {
           timeout = 330;
-          on-timeout = "hyprctl dispatch dpms off || niri msg action power-off-monitors"; # screen off when timeout has passed
-          on-resume = "(hyprctl dispatch dpms on || niri msg action power-on-monitors) && brightnessctl -r"; # screen on when activity is detected after timeout has fired.
-        }
+          on-timeout = "hyprctl dispatch dpms off";
+          on-resume = "(hyprctl dispatch dpms on) && brightnessctl -r";
+        })
+        (lib.mkIf (compositor == "niri") {
+          timeout = 330;
+          on-timeout = "niri msg action power-off-monitors";
+          on-resume = "(niri msg action power-on-monitors) && brightnessctl -r";
+        })
 
         {
           timeout = 1800;
